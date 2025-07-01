8851c843556b4aecbd3624d358dea973d57d3246
Rama Hany <hanyrama703@gmai.com>
2025-07-02 02:07:27 +0300

    made custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 24, top: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCD7A),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              "Flutter Tips",
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                "build your creer with our course",
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.black, size: 32),
+            ),
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              "May 21, 2022",
+              style: TextStyle(color: Colors.black.withValues(alpha: 0.5)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
