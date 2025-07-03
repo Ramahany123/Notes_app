dc0ccb93a2d7783fae1e1ee131626a4236a6bd92
Rama Hany <hanyrama703@gmai.com>
2025-07-03 09:36:03 +0300

    change in path of imports

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constants.dart';
-import 'package:notes_app/views/widgets/add_note_modal_sheet.dart';
-import 'package:notes_app/views/widgets/notes_view_body.dart';
+import 'package:notes_app/widgets/add_note_modal_sheet.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
