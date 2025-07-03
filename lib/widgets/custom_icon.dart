6256ccb5e5b5f45b824d18b6d3da356575854da4
Rama Hany <hanyrama703@gmai.com>
2025-07-03 10:03:44 +0300

    refactoring custom app bar

--- lib/widgets/custom_icon.dart
+++ lib/widgets/custom_icon.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,7 +12,7 @@ class CustomSearchIcon extends StatelessWidget {
         color: Colors.white.withValues(alpha: 0.07),
         borderRadius: BorderRadius.circular(16),
       ),
-      child: Center(child: Icon(Icons.search, size: 27)),
+      child: Center(child: Icon(icon, size: 27)),
     );
   }
 }
