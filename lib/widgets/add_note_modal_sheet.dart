176c16b61a695f88abbe4c751dac358433e74150
Rama Hany <hanyrama703@gmai.com>
2025-07-04 07:22:20 +0300

    refactored form

--- lib/widgets/add_note_modal_sheet.dart
+++ lib/widgets/add_note_modal_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/widgets/custom_button.dart';
-import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddNote extends StatelessWidget {
   const AddNote({super.key});
@@ -9,18 +8,7 @@ class AddNote extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(28.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            CustomTextField(hintText: 'Title'),
-            SizedBox(height: 15),
-            CustomTextField(hintText: 'Content', maxLines: 5),
-            SizedBox(height: 50),
-            CustomButton(),
-            SizedBox(height: 35),
-          ],
-        ),
-      ),
+      child: SingleChildScrollView(child: AddNoteForm()),
     );
   }
 }
