9ab17af5a6d3a5c5dbcb4dde90937cba20f36237
Rama Hany <hanyrama703@gmai.com>
2025-07-06 03:57:22 +0300

    refresh notes after delete and add note

--- lib/widgets/add_note_modal_sheet.dart
+++ lib/widgets/add_note_modal_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/notes/notes_cubit.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddNote extends StatelessWidget {
@@ -14,6 +15,7 @@ class AddNote extends StatelessWidget {
         listener: (context, state) {
           if (state is AddNoteSuccess) {
             Navigator.pop(context);
+            BlocProvider.of<NotesCubit>(context).fetchAllNotes();
           }
           if (state is AddNoteFailure) {
             debugPrint('Failed: ${state.errorMsg}');
