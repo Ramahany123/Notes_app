1e06f9f6486d1f76de7ff8b9463b7d651bb54740
Rama Hany <hanyrama703@gmai.com>
2025-07-06 06:31:12 +0300

    added list for note card colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,13 +4,13 @@ const kPrimaryColor = Color(0xff53EBD6);
 const kNotesBox = 'notes_box';
 
 const List<Color> kNoteColors = [
-  Color(0xFF1F3A93), // Dark blue
-  Color(0xFF2C3E50), // Blue-gray
-  Color(0xFF512E5F), // Deep purple
-  Color(0xFF154360), // Navy teal
-  Color(0xFF4A235A), // Royal plum
-  Color(0xFF784212), // Deep orange-brown
-  Color(0xFF145A32), // Dark forest green
-  Color(0xFF7B241C), // Brick red
-  Color(0xFF4E342E), // Coffee brown
+  Color(0xFF4C6EF5), // Lighter dark blue
+  Color(0xFF5C7080), // Lighter blue-gray
+  Color(0xFF8E44AD), // Lighter purple
+  Color(0xFF2980B9), // Lighter teal/navy
+  Color(0xFF9B59B6), // Lavender plum
+  Color(0xFFCA6F1E), // Lighter orange-brown
+  Color(0xFF27AE60), // Lighter forest green
+  Color(0xFFE74C3C), // Brick red (vibrant but nice on dark)
+  Color(0xFF8D6E63), // Softened coffee brown
 ];
